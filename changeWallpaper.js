580a2fca3c43726ebf428b39d87871efb2c94f77
pathum <awpathum@gmail.com>
2019-11-08 17:02:30 +0530

    image is not loading withjson array

--- changeWallpaper.js
+++
@@ -1,3 +0,0 @@
-function myFunction() {
-    document.body.style.backgroundImage = "url('dingo.jpg')";
-}
--- process.js
+++ process.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.style.backgroundImage = "url('dingo.jpg')";
+    data = '[{"filename": "./backgrounds/lion.jpg","id": "animal01"},{"filename": "./backgrounds/girl.jpg","id": "girl01"},{"filename": "./backgrounds/motivate.jpg","id": "motivation01"},{"filename": "./backgrounds/motivate2.jpg","id": "motivation02"}]';
+    var mydata = JSON.parse(data);
+    var image = mydata[0].filename;
+    alert(mydata[0].filename);
+    //alert(mydata[0].age);
+    //var pets = '{"pets":[{"name":"jack"},{"name":"john"},{"name":"joe"}]}';
+    //var images = '{ "backgrounds": [{"filename": "./backgrounds/lion.jpg","id": "animal01"},{"filename": "./backgrounds/girl.jpg","id": "girl01"},{"filename": "./backgrounds/motivate.jpg","id": "motivation01"},{"filename": "./backgrounds/motivate2.jpg","id": "motivation02"}]}';
+    // var images = '{ {"filename": "./backgrounds/lion.jpg","id": "animal01"},{"filename": "./backgrounds/girl.jpg","id": "girl01"},{"filename": "./backgrounds/motivate.jpg","id": "motivation01"},{"filename": "./backgrounds/motivate2.jpg","id": "motivation02"}}';
+    document.body.style.backgroundImage = "url(image)";
+    // console.log(`I'm ${age} years old!`)
+
+    // var images = JSON.parse(backgrounds);
+    // // var mydata = JSON.parse(data);
+
+    // alert(images[0].filename);
 });
